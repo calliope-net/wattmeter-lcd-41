8140ff4b5f2b60c42abbb012e2c8c898507bc539
calliope-net <141074597+calliope-net@users.noreply.github.com>
2026-04-05 22:03:12 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 lcd.init_display(lcd.eDisplay.qwiic_16_2)
 pins.wattmeter_reset()
 loops.everyInterval(500, function () {
-    lcd.write_text(0, 0, 3, pins.roundWithPrecision(pins.get_bus_voltage_V(), 2))
-    lcd.write_text(0, 5, 5, lcd.lcd_text("V"))
-    lcd.write_text(1, 0, 3, pins.get_current_mA())
-    lcd.write_text(1, 5, 6, lcd.lcd_text("mA"))
+    lcd.write_text(0, 0, 6, pins.get_bus_voltage_V(), lcd.eAlign.left)
+    lcd.write_text(0, 7, 7, lcd.lcd_text("V"))
+    lcd.write_text(0, 9, 12, pins.get_current_mA(), lcd.eAlign.right)
+    lcd.write_text(0, 14, 15, lcd.lcd_text("mA"))
 })
